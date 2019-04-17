70acdd593fa52d6881057dbf0fe3e754de11078c
Damian Wawrzyniak <damianwawrzyniak1@gmail.com>
2019-04-17 21:07:07 +0200

    change main to show example program invocation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
-import com.google.gson.Gson;
-import csp.*;
+import csp.Backtracking;
+import csp.CSP;
+import csp.HTMLPrinter;
+import csp.Result;
 import csp.heuristics.FirstInDomainValueSelection;
 import csp.heuristics.FirstUnassignedVariableSelection;
 import csp.heuristics.LCVValueSelection;
 import csp.heuristics.MRVVariableSelection;
 import csp.loader.Loader;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +19,14 @@ public class Main {
         Backtracking withHeuristics = new Backtracking(new MRVVariableSelection(), new LCVValueSelection());
         Backtracking withoutHeuristics = new Backtracking(new FirstUnassignedVariableSelection(), new FirstInDomainValueSelection());
 
-        String filename = "test_futo_9_0.txt";
+        String filename = "test_futo_8_0.txt";
         CSP board = loader.load(filename);
         List<Result> results = new ArrayList<>();
 
-        results.add(withHeuristics.searchWithForwardChecking(board));
-//        results.add(withHeuristics.search(board));
-//        results.add(withoutHeuristics.search(board));
+        results.add(withoutHeuristics.search(board));
         results.add(withoutHeuristics.searchWithForwardChecking(board));
+        results.add(withHeuristics.searchWithForwardChecking(board));
+        results.add(withHeuristics.search(board));
 
         new HTMLPrinter().printHtml(results, filename);
     }
