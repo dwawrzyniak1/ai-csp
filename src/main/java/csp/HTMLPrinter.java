489c65a0f0c8662e98ce6eaa864815af40947bca
Damian Wawrzyniak <damianwawrzyniak1@gmail.com>
2019-04-17 20:41:47 +0200

    removed unused methods, changed methods order to improve readability

--- src/main/java/csp/HTMLPrinter.java
+++ src/main/java/csp/HTMLPrinter.java
@@ -5,23 +5,6 @@ import java.util.List;
 
 public class HTMLPrinter {
 
-    private static final String FC = "FC  ";
-    private static final int FC_INDEX_0 = 6;
-    private static final int FC_INDEX_1 = 7;
-
-    private static final String BT = "BT  ";
-    private static final int BT_INDEX_0 = 2;
-    private static final int BT_INDEX_1 = 3;
-
-    private static final String BT_HEUR = "BT MRV LCV";
-    private static final int BT_HEUR_INDEX_0 = 4;
-    private static final int BT_HEUR_INDEX_1 = 5;
-
-    private static final String FC_HEUR = "FC MRV LCV";
-    private static final int FC_HEUR_INDEX_0 = 8;
-    private static final int FC_HEUR_INDEX_1 = 9;
-
-
     private static String startOfFile = "<!doctype html>\n" +
             "\n" +
             "        <html lang=\"en\">\n" +
@@ -55,54 +38,17 @@ public class HTMLPrinter {
             "liczba iteracji"
     };
 
-    public void printHtml(List<Result> results, String file){
+    public void printHtml(List<Result> results, String filename){
         StringBuilder builder = new StringBuilder(startOfFile);
-        builder.append("<h1>").append(file).append("</h1>");
+        builder.append("<h1>").append(filename).append("</h1>");
+
         appendStatistics(results, builder);
         appendResult(results.get(0), builder);
-        appendInitialState(file, builder);
+        appendInitialState(filename, builder);
 
         builder.append(endOfFile);
 
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file + ".html"))){
-
-            writer.write(builder.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void appendInitialState(String file, StringBuilder builder) {
-        builder.append("<h3>").append("Stan początkowy: ").append("</h3>");
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + file)))) {
-
-            String line;
-            while((line = reader.readLine()) != null){
-                builder.append(line).append("<br>");
-            }
-
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    private void appendResult(Result result, StringBuilder builder) {
-        builder.append("<h2>").append("Rozwiazania: ").append("</h2>");
-        for(Variable[][] solution : result.getSolutions()){
-            builder.append("<table class=\"table\">");
-
-            for(Variable[] row : solution){
-                builder.append("<tr>");
-                for(Variable column : row){
-                    builder.append("<td>").append(column.getValue()).append("</td>");
-                }
-                builder.append("</tr>");
-            }
-
-            builder.append("</table>");
-        }
+        saveToFile(filename, builder);
     }
 
     private void appendStatistics(List<Result> results, StringBuilder builder) {
@@ -126,55 +72,50 @@ public class HTMLPrinter {
         builder.append("</tr>");
     }
 
-    private void appendResultHTML(StringBuilder builder, String filename) {
-        List<Result> results = ResultFactory.getResults(filename);
-        String[] display = new String[3]; // nazwa pliku, rozwiazanie plus sekundy i iteracje dla kazdej metody
-        for(Result result : results){
-            appendRowElement(result, display);
-        }
-
-        builder.append("\t<tr>\n");
-        for(String rowElement : display){
-            appendRowElementToHtml(builder, rowElement);
-        }
-        builder.append("\t</tr>\n");
-    }
-
-    private void appendRowElement(Result result, String[] display) {
-        switch (result.getAlgorithmName()){
-            case BT:
-                display[BT_INDEX_0] = String.valueOf(result.getAmountOfTimeInSeconds());
-                display[BT_INDEX_1] = String.valueOf(result.getNumberOfIterations());
-                return;
-            case BT_HEUR:
-                display[BT_HEUR_INDEX_0] = String.valueOf(result.getAmountOfTimeInSeconds());
-                display[BT_HEUR_INDEX_1] = String.valueOf(result.getNumberOfIterations());
-                return;
-            case FC:
-                display[FC_INDEX_0] = String.valueOf(result.getAmountOfTimeInSeconds());
-                display[FC_INDEX_1] = String.valueOf(result.getNumberOfIterations());
-                return;
-            case FC_HEUR:
-                display[FC_HEUR_INDEX_0] = String.valueOf(result.getAmountOfTimeInSeconds());
-                display[FC_HEUR_INDEX_1] = String.valueOf(result.getNumberOfIterations());
-                return;
-            default:
-                System.err.println("Blad dopasowania rozwiazania");
-                return;
-        }
-    }
-
-    private void appendSolutionToHTMLTableRow(StringBuilder builder, List<Variable[][]> solutions) {
-        builder.append("\t<td>\n");
-
-        builder.append("<span>").append(solutions.size()).append("</span>");
-
-        builder.append("\t</td>\n");
-    }
-
     private void appendRowElementToHtml(StringBuilder builder, String content) {
         builder.append("<td>").append(content).append("</td>");
     }
 
+    private void appendResult(Result result, StringBuilder builder) {
+        builder.append("<h2>").append("Rozwiazania: ").append("</h2>");
+        for(Variable[][] solution : result.getSolutions()){
+            builder.append("<table class=\"table\">");
+
+            for(Variable[] row : solution){
+                builder.append("<tr>");
+                for(Variable column : row){
+                    builder.append("<td>").append(column.getValue()).append("</td>");
+                }
+                builder.append("</tr>");
+            }
+
+            builder.append("</table>");
+        }
+    }
+
+    private void appendInitialState(String file, StringBuilder builder) {
+        builder.append("<h3>").append("Stan początkowy: ").append("</h3>");
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/" + file)))) {
+
+            String line;
+            while((line = reader.readLine()) != null){
+                builder.append(line).append("<br>");
+            }
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void saveToFile(String filename, StringBuilder builder) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename + ".html"))){
+
+            writer.write(builder.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
